efaa2019843218a81e25ec6a9ce9a3fdc8ef54ef
ll3971287-glitch <ll3971287@gmail.com>
2026-09-13 17:59:42 +0800

    add src/main.js

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { router } from './router.js'
+import './styles/global.css'
+
+createApp(App).use(router).mount('#app')
+
+if ('serviceWorker' in navigator && import.meta.env.PROD) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js').catch(() => {})
+  })
+}
