ed4763e072dd15db835d7556fb8c1b37e767f8d9
ll3971287-glitch <ll3971287@gmail.com>
2026-09-13 17:59:38 +0800

    add src/components/ui/TagChips.vue

---
+++ src/components/ui/TagChips.vue
@@ -0,0 +1,17 @@
+<template>
+  <span v-for="tg in tags" :key="tg.id" class="tag-chip" :style="{ background: tg.color }">
+    {{ tg.name }}
+  </span>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { store } from '../../store.js'
+import { tagsOf } from '../../selectors.js'
+
+const props = defineProps({
+  tagIds: { type: Array, default: () => [] },
+})
+
+const tags = computed(() => tagsOf(store.state, props.tagIds))
+</script>
