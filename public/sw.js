0ba3a5cf9e4191e3c94f9b5075771920bdce45cd
ll3971287-glitch <ll3971287@gmail.com>
2026-09-13 17:58:36 +0800

    add public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'uls-v2'
+const CORE = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png']
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches
+      .open(CACHE)
+      .then((c) => c.addAll(CORE))
+      .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  )
+})
+
+// 网络优先：有网时永远拿最新版本并回填缓存；断网时回退到缓存（保证离线可用）
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url)
+  if (e.request.method !== 'GET' || url.origin !== location.origin) return
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        if (res && res.ok) {
+          const clone = res.clone()
+          caches.open(CACHE).then((c) => c.put(e.request, clone))
+        }
+        return res
+      })
+      .catch(() => caches.match(e.request).then((hit) => hit || caches.match('./index.html')))
+  )
+})
