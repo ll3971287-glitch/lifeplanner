79b13eac27ee1229177f941e398ebab9df0c28f0
ll3971287-glitch <ll3971287@gmail.com>
2026-09-18 21:24:29 +0800

    update src/views/GoalsView.vue

---
+++ src/views/GoalsView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="goals-view">
+    <GoalsPanel standalone />
+  </div>
+</template>
+
+<script setup>
+import GoalsPanel from '../components/goal/GoalsPanel.vue'
+</script>
+
+<style scoped>
+.goals-view {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+}
+</style>
